baf171972eded4bb1fba37c9d402e06e1a99365c
jaykishansolanki <jaykishansolanki@gmail.com>
2019-06-20 12:30:04 +0530

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "practical_test",
+
+    'summary': """
+        PRACTICAL
+        """,
+
+    # any module necessary for this one to work correctly
+    'depends': ['sale'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        # 'data/ir_sequence_data.xml',
+        'views/views.xml',
+        # 'views/templates.xml',
+        # 'views/partner.xml',
+        # 'report/report_templates.xml',
+        # 'report/employee_report_view.xml',
+    ],
+   
+}
